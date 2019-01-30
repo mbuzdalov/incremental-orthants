85a3d2bc5628b70fd23feea03464647a8f40f778
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-01-30 19:25:47 +0300

    Add a benchmark on insertion of random points.

---
+++ benchmarking/src/main/scala/ru/ifmo/iorthant/jmh/IBEAFitnessRandomBenchmark.scala
@@ -0,0 +1,75 @@
+package ru.ifmo.iorthant.jmh
+
+import java.util.Random
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
+import ru.ifmo.iorthant.ibea.{NaiveImplementation, OrthantImplementation}
+import ru.ifmo.iorthant.util.Syntax._
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Timeout(time = 1, timeUnit = TimeUnit.HOURS)
+@Warmup(iterations = 1, time = 6)
+@Measurement(iterations = 1, time = 1)
+@Fork(value = 5)
+class IBEAFitnessRandomBenchmark {
+
+  //noinspection VarCouldBeVal: this inspection shall be suppressed for everything @Param
+  @Param(Array("10", "31", "100", "316", "1000"))
+  private var n: Int = _
+
+  //noinspection VarCouldBeVal: this inspection shall be suppressed for everything @Param
+  @Param(Array("2", "3", "5", "7", "10"))
+  private var d: Int = _
+
+  //noinspection VarCouldBeVal: this inspection shall be suppressed for everything @Param
+  @Param(Array("naive", "orthant"))
+  private var algorithm: String = _
+
+  //noinspection VarCouldBeVal: this inspection shall be suppressed for everything @Param
+  @Param(Array("plane", "cube", "line"))
+  private var test: String = _
+
+  private var instances: Array[Array[Array[Double]]] = _
+
+  private def generatePoint(rng: Random): Array[Double] = test match {
+    case "cube" =>
+      Array.fill(d)(rng.nextDouble())
+    case "line" =>
+      val v = rng.nextDouble()
+      Array.fill(d)(v)
+    case "plane" =>
+      Array.fill(d)(rng.nextDouble()).whereAlso(a => a(0) += 1.0 - a.sum)
+  }
+
+  @Setup
+  def initialize(): Unit = instances = Array.tabulate(10) { i =>
+    // intentionally do not depend on "algorithm"
+    val rng = new Random(i * 72433566236111L + n * 623432 + d * 91274635553235L + test.hashCode)
+    val actions = Array.newBuilder[Array[Double]]
+    for (_ <- 0 until 10 * n) {
+      actions += generatePoint(rng)
+    }
+    actions.result()
+  }
+
+  @Benchmark
+  def benchmark(bh: Blackhole): Unit = {
+    val maxSize = n * 2
+    for (instance <- instances) {
+      val a = algorithm match {
+        case "naive" => new NaiveImplementation[Unit](0.05, maxSize)
+        case "orthant" => new OrthantImplementation[Unit](0.05, maxSize, d)
+      }
+      for (point <- instance) {
+        a.addIndividual((), point)
+        if (a.size == maxSize) {
+          a.trimPopulation(n)
+        }
+      }
+    }
+  }
+}
