ff82631453e1db974f20b09798bc7a4674ea7668
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-03-26 21:33:17 +0300

    Get rid of negated array creation by making two near-copies of trees.
    There are now an increasing tree and a decreasing tree. Their code
    is largely the same except for four locations. I made several attempts
    to make an implementation without code duplicates and failed.
    The current approach is to some extent ugly,
    but it is faster by ~10% than the previous version.

--- implementations/src/main/scala/ru/ifmo/iorthant/noq2d/SimpleKD.scala
+++ implementations/src/main/scala/ru/ifmo/iorthant/noq2d/SimpleKD.scala
@@ -1,6 +1,7 @@
 package ru.ifmo.iorthant.noq2d
 
 import ru.ifmo.iorthant.util._
+import ru.ifmo.iorthant.util.kd.{DecreasingTree, IncreasingTree}
 
 class SimpleKD[@specialized(Specialization.defaultSet) T](minNonStrictCoordinate: Int)(implicit m: Monoid[T])
   extends NoUpdateIncrementalOrthantSearch[T] {
@@ -11,13 +12,13 @@ class SimpleKD[@specialized(Specialization.defaultSet) T](minNonStrictCoordinate
   override def newDataPointHandleArray(n: Int): Array[DataPointHandle] = new Array[DataPointHandle](n)
   override def newQueryPointHandleArray(n: Int): Array[QueryPointHandle] = new Array[QueryPointHandle](n)
 
-  private var dataPoints: KDTree[DataPointHandle] = KDTree.empty
-  private var queryPoints: KDTree[QueryPointHandle] = KDTree.empty
+  private var dataPoints: KDTree[DataPointHandle] = IncreasingTree.empty
+  private var queryPoints: KDTree[QueryPointHandle] = DecreasingTree.empty
 
   override def addDataPoint(point: Array[Double], value: T): DataPointHandle = {
     val handle = new DataWrapper[T](point, value)
     dataPoints = dataPoints.add(point, handle)
-    queryPoints.forDominating(new SimpleKD.AddContext[T](Arrays.negate(point), value, minNonStrictCoordinate))
+    queryPoints.forDominating(new SimpleKD.AddContext[T](point, value, minNonStrictCoordinate))
     handle
   }
 
@@ -25,7 +26,7 @@ class SimpleKD[@specialized(Specialization.defaultSet) T](minNonStrictCoordinate
                                 tracker: NoUpdateIncrementalOrthantSearch.UpdateTracker[T, I],
                                 identifier: I): QueryPointHandle = {
     val value = makeQuery(point)
-    val handle = new QueryWrapper.Tracking(Arrays.negate(point), value, tracker, identifier)
+    val handle = new QueryWrapper.Tracking(point, value, tracker, identifier)
     queryPoints = queryPoints.add(handle.point, handle)
     handle
   }
@@ -39,7 +40,7 @@ class SimpleKD[@specialized(Specialization.defaultSet) T](minNonStrictCoordinate
   override def removeDataPoint(handle: DataPointHandle)
                               (implicit hm: HasNegation[T]): Unit = {
     dataPoints = dataPoints.remove(handle.point, handle)
-    queryPoints.forDominating(new SimpleKD.RemoveContext[T](Arrays.negate(handle.point), handle.value, minNonStrictCoordinate))
+    queryPoints.forDominating(new SimpleKD.RemoveContext[T](handle.point, handle.value, minNonStrictCoordinate))
   }
 
   override def removeQueryPoint(handle: QueryPointHandle): Unit = {
@@ -48,8 +49,9 @@ class SimpleKD[@specialized(Specialization.defaultSet) T](minNonStrictCoordinate
 }
 
 object SimpleKD {
-  class QueryContext[@specialized(Specialization.defaultSet) T](val point: Array[Double], minNonStrictCoordinate: Int)
-                                                               (implicit m: Monoid[T])
+  private class QueryContext[@specialized(Specialization.defaultSet) T](val point: Array[Double],
+                                                                        minNonStrictCoordinate: Int)
+                                                                       (implicit m: Monoid[T])
     extends KDTree.TraverseContext[DataWrapper[T]] {
     private var v = m.zero
     def value: T = v
@@ -61,25 +63,27 @@ object SimpleKD {
     }
   }
 
-  class AddContext[@specialized(Specialization.defaultSet) T](val point: Array[Double], val value: T, minNonStrictCoordinate: Int)
-                                                             (implicit m: Monoid[T])
+  private class AddContext[@specialized(Specialization.defaultSet) T](val point: Array[Double], val value: T,
+                                                                      minNonStrictCoordinate: Int)
+                                                                     (implicit m: Monoid[T])
     extends KDTree.TraverseContext[QueryWrapper[T]] {
     override def update(data: QueryWrapper[T]): Unit = {
       data.plus(value)
     }
     override def isDominatedBy(point: Array[Double]): Boolean = {
-      Dominance.partial(point, this.point, minNonStrictCoordinate)
+      Dominance.partial(this.point, point, minNonStrictCoordinate)
     }
   }
 
-  class RemoveContext[@specialized(Specialization.defaultSet) T](val point: Array[Double], val value: T, minNonStrictCoordinate: Int)
-                                                                (implicit m: HasNegation[T])
+  private class RemoveContext[@specialized(Specialization.defaultSet) T](val point: Array[Double], val value: T,
+                                                                         minNonStrictCoordinate: Int)
+                                                                        (implicit m: HasNegation[T])
     extends KDTree.TraverseContext[QueryWrapper[T]] {
     override def update(data: QueryWrapper[T]): Unit = {
       data.minus(value)
     }
     override def isDominatedBy(point: Array[Double]): Boolean = {
-      Dominance.partial(point, this.point, minNonStrictCoordinate)
+      Dominance.partial(this.point, point, minNonStrictCoordinate)
     }
   }
 }
---
+++ implementations/src/main/scala/ru/ifmo/iorthant/util/kd/DecreasingTree.scala
@@ -0,0 +1,139 @@
+package ru.ifmo.iorthant.util.kd
+
+import java.util.concurrent.ThreadLocalRandom
+
+import ru.ifmo.iorthant.util.{Arrays, KDTree}
+import ru.ifmo.iorthant.util.KDTree.TraverseContext
+
+import scala.collection.mutable.ArrayBuffer
+
+/*
+ * Increasing and decreasing trees are different only in few locations (sign changed). This is for performance. Sorry.
+ */
+object DecreasingTree {
+  private def chooseDifferentCoordinate(a: Array[Double], b: Array[Double], first: Int): Int = {
+    if (a(first) != b(first)) {
+      first
+    } else {
+      chooseDifferentCoordinate(a, b, (first + 1) % a.length)
+    }
+  }
+
+  private def createSeparator(a: Double, b: Double): Double = {
+    a + (b - a) * ThreadLocalRandom.current().nextDouble()
+  }
+
+  private class Empty[D] extends KDTree[D] {
+    override protected[util] def addImpl(point: Array[Double], data: D, index: Int): KDTree[D] = new Leaf(point, data)
+    override protected[util] def forDominatingImpl(ctx: TraverseContext[D], mask: Int): Unit = {}
+    override def remove(point: Array[Double], data: D): KDTree[D] = {
+      throw new IllegalArgumentException("No point can be in an empty KDTree")
+    }
+    override def isEmpty: Boolean = true
+  }
+
+  private val emptyInstance = new Empty[Nothing]
+
+  def empty[D]: KDTree[D] = emptyInstance.asInstanceOf[Empty[D]]
+
+  private class Branch[D](index: Int,
+                          value: Double,
+                          private[this] var left: KDTree[D],
+                          private[this] var right: KDTree[D]) extends KDTree[D] {
+    override protected[util] def addImpl(point: Array[Double], data: D, index: Int): KDTree[D] = {
+      if (point(this.index) >= value) {
+        left = left.addImpl(point, data, (this.index + 1) % point.length)
+        this
+      } else {
+        right = right.addImpl(point, data, (this.index + 1) % point.length)
+        this
+      }
+    }
+
+    override protected[util] def forDominatingImpl(ctx: TraverseContext[D], mask: Int): Unit = {
+      val bit = 1 << index
+      if ((mask & bit) == 0) {
+        left.forDominatingImpl(ctx, mask)
+        right.forDominatingImpl(ctx, mask)
+      } else {
+        if (ctx.point(index) < value) {
+          left.forDominatingImpl(ctx, mask & ~bit)
+          right.forDominatingImpl(ctx, mask)
+        } else {
+          left.forDominatingImpl(ctx, mask)
+        }
+      }
+    }
+
+    override def remove(point: Array[Double], data: D): KDTree[D] = {
+      if (point(index) >= value) {
+        left = left.remove(point, data)
+        if (left.isEmpty) right else this
+      } else {
+        right = right.remove(point, data)
+        if (right.isEmpty) left else this
+      }
+    }
+
+    override def isEmpty: Boolean = false
+  }
+
+  private class Leaf[D](point: Array[Double], private[this] var data0: D) extends KDTree[D] {
+    private[this] var dataMore: ArrayBuffer[D] = _
+
+    override protected[util] def addImpl(point: Array[Double], data: D, index: Int): KDTree[D] = {
+      if (Arrays.equal(this.point, point)) {
+        if (dataMore == null) {
+          dataMore = new ArrayBuffer[D](2)
+        }
+        dataMore += data
+        this
+      } else {
+        val idx = chooseDifferentCoordinate(point, this.point, index)
+        val thisV = this.point(idx)
+        val thatV = point(idx)
+        val sep = createSeparator(thisV, thatV)
+        val that = new Leaf(point, data)
+        if (thisV > thatV) {
+          new Branch(idx, sep, this, that)
+        } else {
+          new Branch(idx, sep, that, this)
+        }
+      }
+    }
+
+    override protected[util] def forDominatingImpl(ctx: TraverseContext[D], mask: Int): Unit = {
+      if (mask == 0 || ctx.isDominatedBy(point)) {
+        ctx.update(data0)
+        if (dataMore != null) {
+          for (d <- dataMore) ctx.update(d)
+        }
+      }
+    }
+
+    override def remove(point: Array[Double], data: D): KDTree[D] = {
+      if (data0 == data) {
+        if (dataMore == null) {
+          empty
+        } else {
+          val lastIndex = dataMore.size - 1
+          data0 = dataMore(lastIndex)
+          dataMore.remove(lastIndex)
+          if (dataMore.isEmpty) {
+            dataMore = null
+          }
+          this
+        }
+      } else {
+        val idx = dataMore.indexOf(data)
+        dataMore.remove(idx)
+        if (dataMore.isEmpty) {
+          dataMore = null
+        }
+        this
+      }
+    }
+
+    override def isEmpty: Boolean = false
+  }
+}
